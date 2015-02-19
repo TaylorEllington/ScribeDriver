003871acf27822f43639a0e71842ec73e6f9772b
Taylor Ellington <taylorellington101@gmail.com>
2015-02-19 13:24:34 -0600

    Preliminary UI styleing

---
+++ ScribeDriver/LayoutProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScribeDriver
+{
+    class LayoutProfile
+    {
+        String profileName;
+        int vCount;
+        int hCount;
+        double hBorder;
+        double vBorder;
+        double hSpacing;
+        double vSpacing;
+        double chipWidth;
+        double chipHeight;
+
+       
+    }
+}
