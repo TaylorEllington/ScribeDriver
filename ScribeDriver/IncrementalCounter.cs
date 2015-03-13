216d8d3f910917f5bd7687d8c1a65c121088b474
Taylor Ellington <taylorellington101@gmail.com>
2015-03-13 14:00:35 -0500

    Added incrementalCounter basic functions and comments

--- ScribeDriver/IncrementalCounter.cs
+++ ScribeDriver/IncrementalCounter.cs
@@ -8,5 +8,30 @@ namespace ScribeDriver
 {
     class incrementalCounter
     {
+        private int startValue;
+        private int currentValue;
+        
+        void seedCounter(int seed){
+            //Accepts the first value (inclusive) to be scribed
+            startValue = seed;
+            currentValue = seed;
+        }
+        string getValueAsHexString(){
+            //returns a Hex representation of the current value to be scribed
+            return currentValue.ToString("X");
+        }
+        int getValueAsInt(){
+             
+            return currentValue;
+        }
+        void next(){
+            //Increments the counter
+            currentValue++;
+        }
+        void skip(int numberOfSkips){
+            //Used only for resuming. This serves as a reset for currentValue
+            currentValue = startValue + numberOfSkips;
+        }
+
     }
 }
