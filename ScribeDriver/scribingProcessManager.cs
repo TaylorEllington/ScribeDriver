a9dfc5e9ff11779a151787e449b0646ffbbd597c
Taylor Ellington <taylorellington101@gmail.com>
2015-03-14 02:40:25 -0500

    added function declarations to scribingProcessManager

--- ScribeDriver/scribingProcessManager.cs
+++ ScribeDriver/scribingProcessManager.cs
@@ -8,5 +8,29 @@ namespace ScribeDriver
 {
     class scribingProcessManager
     {
+        bool runFlag;
+
+        scribingProcessManager() { }
+
+        void drawZero() { }
+        void drawOne() { }
+        void drawTwo() { }
+        void drawThree() { }
+        void drawFour() { }
+        void drawFive() { }
+        void drawSix() { }
+        void drawSeven() { }
+        void drawEight() { }
+        void drawNine() { }
+        void drawA() { }
+        void drawB() { }
+        void drawC() { }
+        void drawD() { }
+        void drawE() { }
+        void drawF() { }
+
+        void stopRun() { }
+        void run() { }
+
     }
 }
